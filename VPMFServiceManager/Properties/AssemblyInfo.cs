ba5bb5bceb8507c58a8ca5df366a2dba01372905
Capizzi Filippo <capizzi@rsaweb.com>
2023-11-17 18:07:12 +0100

    Aggiornata versione

--- VPMFServiceManager/Properties/AssemblyInfo.cs
+++ VPMFServiceManager/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.023.01.0")]
-[assembly: AssemblyFileVersion("2.022.01.0")]
+[assembly: AssemblyVersion("2.023.11.0")]
+[assembly: AssemblyFileVersion("2.022.11.0")]
